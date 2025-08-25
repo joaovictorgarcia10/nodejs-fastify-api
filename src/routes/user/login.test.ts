d90336d81ccfabb1d65587a78a7ec82b93a097de
João Victor Garcia <jvictor_sk8@hotmail.com>
2025-08-25 07:32:47 -0300

    add login test

---
+++ src/routes/user/login.test.ts
@@ -0,0 +1,21 @@
+import { test, expect } from 'vitest'
+import request from 'supertest'
+import { server } from '../../app'
+import { makeUser } from '../../../tests/factories/make-user'
+
+test('login', async () => {
+    await server.ready()
+
+    const { user, passwordBeforeHash } = await makeUser()
+
+    const response = await request(server.server)
+        .post('/login')
+        .set('Content-Type', 'application/json')
+        .send({
+            email: user.email,
+            password: passwordBeforeHash,
+        })
+
+    expect(response.status).toEqual(200)
+    expect(response.body).toEqual({ token: expect.any(String) })
+})
